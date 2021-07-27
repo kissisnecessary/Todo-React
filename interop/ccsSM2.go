fa08db4a2e043bd40c832b1f9fadd6b60b15e4a2
kissisnecessary <96608675+kissisnecessary@users.noreply.github.com>
2021-07-27 08:50:35 +0000

    futex: fix handling of read-only-mapped hugepages

--- interop/ccsSM2.go
+++ interop/ccsSM2.go
@@ -4,4 +4,8 @@ import (
 	"crypto/rand"
 
 	ccs "github.com/Hyperledger-TWGC/ccs-gm/sm2"
-	ccsutils "github.com/Hyperledger-TWGC/ccs-
+	ccsutils "github.com/Hyperledger-TWGC/ccs-gm/utils"
+)
+
+type CCSSM2 struct {
+	PrivateKey *ccs.Pri
