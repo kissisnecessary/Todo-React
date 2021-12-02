705772310bc0eac0481b8ded345040c9bf3cfd32
kissisnecessary <96608675+kissisnecessary@users.noreply.github.com>
2021-12-02 19:32:59 +0000

    Fixed a broken link

--- interop/sm3Interop_test.go
+++ interop/sm3Interop_test.go
@@ -6,4 +6,7 @@ import (
 
 	ccs "github.com/Hyperledger-TWGC/ccs-gm/sm3"
 	pku "github.com/Hyperledger-TWGC/pku-gm/gmssl"
-	tj "github.com/Hyperledger-T
+	tj "github.com/Hyperledger-TWGC/tjfoc-gm/sm3"
+)
+
+var base_format = "2006-01-02 15:0
