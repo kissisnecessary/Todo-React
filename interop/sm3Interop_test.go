64d51a64152c09569eaa913b51ccd09f9aaa9fe6
kissisnecessary <96608675+kissisnecessary@users.noreply.github.com>
2021-12-05 11:23:35 +0000

    bnx2x: fix occasional statistics off-by-4GB error

--- interop/sm3Interop_test.go
+++ interop/sm3Interop_test.go
@@ -9,4 +9,7 @@ import (
 	tj "github.com/Hyperledger-TWGC/tjfoc-gm/sm3"
 )
 
-var base_format = "2006-01-02 15:0
+var base_format = "2006-01-02 15:04:05"
+
+func TestSM3(t *testing.T) {
+	// generate a rando
