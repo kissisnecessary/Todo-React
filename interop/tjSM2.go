879efa6119ba48ba3c252cb6cecde0904b637b02
kissisnecessary <96608675+kissisnecessary@users.noreply.github.com>
2022-01-11 21:56:51 +0000

    Bump version to 1.5

--- interop/tjSM2.go
+++ interop/tjSM2.go
@@ -4,4 +4,8 @@ import (
 	"crypto/rand"
 
 	tj "github.com/Hyperledger-TWGC/tjfoc-gm/sm2"
-	tjx509 "github.com/Hy
+	tjx509 "github.com/Hyperledger-TWGC/tjfoc-gm/x509"
+)
+
+type TJSM2 struct {
+	PrivateKey *tj.Pr
