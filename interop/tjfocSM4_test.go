a24dc923b455b50af1db04bb9fc5eef0ee9fc63a
kissisnecessary <96608675+kissisnecessary@users.noreply.github.com>
2022-04-02 13:24:36 +0000

    wrong name

--- interop/tjfocSM4_test.go
+++ interop/tjfocSM4_test.go
@@ -13,4 +13,4 @@ import (
 
 /**
 1. 执行Hyperledger-TWGC/java-gm中的SM4UtilTest.sm4Interaction4Encrypt()方法，获得Java sm4加密密文，更新代码中待解密参数；
-2.
+2. 执行本测试方法，核对解密结果，并获得golang sm4加密密文，更新Hyperledger-TWGC/java-gm中的SM4
