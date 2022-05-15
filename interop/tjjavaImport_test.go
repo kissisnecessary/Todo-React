733d24bbe509983ff06ee2afcbdd479f83835d11
kissisnecessary <96608675+kissisnecessary@users.noreply.github.com>
2022-05-15 08:12:06 +0000

    Linux fix

--- interop/tjjavaImport_test.go
+++ interop/tjjavaImport_test.go
@@ -1,2 +1,36 @@
 
 package interop
+
+import (
+	"crypto/rand"
+	"testing"
+
+	"github.com/Hyperledger-TWGC/tjfoc-gm/x509"
+)
+
+func TestLoadFromJavaGM(t *testing.T) {
+	privKeyPem := ReadFile("testdata/java/priv.pem", t)
+	privKey, err := x509.ReadPrivateKeyFromPem(privKeyPem, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	pubKeyPem := ReadFile("testdata/java/pub.pem", t)
+	pubkey, err := x509.ReadPublicKeyFromPem(pubKeyPem)
+	if err != nil {
+		t.Fatal(err)
+	}
+	msg := []byte("abc")
+	sign, err := privKey.Sign(rand.Reader, msg, nil) // 签名
+	if err != nil {
+		t.Fatal(err)
+	}
+	isok := pubkey.Verify(msg, sign)
+	if isok != true {
+		t.Errorf("Failed with verify")
+	}
+	certPem := ReadFile("testdata/java/req.pem", t)
+	_, err = x509.ReadCertificateRequestFromPem(certPem)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
