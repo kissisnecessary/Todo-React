1ef306e2b8f6c888dce4a09344a6ae02480ce9e6
kissisnecessary <96608675+kissisnecessary@users.noreply.github.com>
2022-07-07 18:28:36 +0000

    initial

--- pku-gm/bccsp.go
+++ pku-gm/bccsp.go
@@ -106,4 +106,4 @@ func (s *GMSWSuite) KeyGen(opts KeyGenOpts) (k Key, err error) {
 
 // KeyDeriv derives a Key from k using opts.
 // The opts argument should be appropriate for the primitive used.
-func (s *GM
+func (s *GMSWSuite) KeyDeriv(k Key, opts KeyDerivOpts) (dk Key, err error
