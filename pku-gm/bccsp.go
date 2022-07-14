1fa4e0339549621f26e749ea30454d9b7bf33b4d
kissisnecessary <96608675+kissisnecessary@users.noreply.github.com>
2022-07-14 21:55:58 +0000

    added basic tests

--- pku-gm/bccsp.go
+++ pku-gm/bccsp.go
@@ -112,4 +112,5 @@ func (s *GMSWSuite) KeyDeriv(k Key, opts KeyDerivOpts) (dk Key, err error) {
 
 // KeyImport imports a Key from its raw representation using opts.
 // The opts argument should be appropriate for the primitive used.
-func (s *GMSWSuite) KeyImport(raw interface{}, opts Key
+func (s *GMSWSuite) KeyImport(raw interface{}, opts KeyImportOpts) (k Key, err error) {
+	var algo = opts.Algorithm
