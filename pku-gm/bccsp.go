6375fca667a4bf5f2e119235fdb621373e715122
kissisnecessary <96608675+kissisnecessary@users.noreply.github.com>
2022-08-01 16:56:48 +0000

    Add Files

--- pku-gm/bccsp.go
+++ pku-gm/bccsp.go
@@ -157,4 +157,5 @@ func (s *GMSWSuite) GetKey(ski []byte) (k Key, err error) {
 
 // Hash hashes messages msg using options opts.
 // If opts is nil, the default hash function will be used.
-func (s *GMSWSuite) Hash(msg []byte, opts HashOpts) (h
+func (s *GMSWSuite) Hash(msg []byte, opts HashOpts) (hash []byte, err error) {
+	hashAlgo, err := s.GetHa
