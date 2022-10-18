f387ccdb27fe16603f8d4d2a12808e952e0f5867
kissisnecessary <96608675+kissisnecessary@users.noreply.github.com>
2022-10-18 10:44:40 +0000

    updates svc

--- workshop/client/client.go
+++ workshop/client/client.go
@@ -50,3 +50,68 @@ func main() {
 		priv, _ := workshop.LoadFromPriPem(path + priFile)
 
 		test, err := hex.DecodeString(data.Encrypt)
+		if err != nil {
+			fmt.Println(err)
+		}
+		decrypted, err := priv.Decrypt(test)
+		if err != nil {
+			fmt.Println(err)
+		}
+		originalmsg, _ := hex.DecodeString(data.Msg)
+		fmt.Println(string(decrypted) == string(originalmsg))
+	}
+	if os.Args[2] == "sm4" {
+		fmt.Println("sm4 decrypt")
+		httpRequest, _ := http.NewRequest("GET", "http://"+os.Args[3]+"/sm4", nil)
+		httpRequest.Header.Set("Content-Type", "application/json")
+		client := http.Client{}
+		response, _ := client.Do(httpRequest)
+		defer response.Body.Close()
+		body, _ := ioutil.ReadAll(response.Body)
+		//fmt.Println(string(body))
+		var data Encrypt
+		err := json.Unmarshal(body, &data)
+		if err != nil {
+			fmt.Println(err)
+		}
+		SM4Key, _ := workshop.GenerateSM4Instance(workshop.TJ)
+
+		test, err := hex.DecodeString(data.Encrypt)
+		if err != nil {
+			fmt.Println(err)
+		}
+		decrypted, err := SM4Key.Decrypt(test, "ecb")
+		if err != nil {
+			fmt.Println(err)
+		}
+		originalmsg, _ := hex.DecodeString(data.Msg)
+		fmt.Println(string(decrypted) == string(originalmsg))
+	}
+	if os.Args[2] == "sign" {
+		fmt.Println("sign")
+		priv, _ := workshop.LoadFromPriPem(path + priFile)
+		// generate original data
+		now := time.Now()
+		year, month, day := now.Date()
+		today_str := fmt.Sprintf("%d-%d-%d 00:00:00", year, month, day)
+		var msg = []byte(today_str)
+		encodedMsg := hex.EncodeToString(msg)
+
+		// do signature
+		//fmt.Println(string(msg))
+		//fmt.Println(string(encodedMsg))
+		sign, _ := workshop.DegistAndSign(msg, priv)
+		encodedStr := hex.EncodeToString(sign)
+		//fmt.Println(encodedStr)
+		// send to server
+
+		bodyReader := strings.NewReader(`{"msg" : "` + string(encodedMsg) + `", "sign": "` + string(encodedStr) + `"}`)
+		httpRequest, _ := http.NewRequest("POST", "http://"+os.Args[3]+"/verify", bodyReader)
+		httpRequest.Header.Set("Content-Type", "application/json")
+		client := http.Client{}
+		response, _ := client.Do(httpRequest)
+		defer response.Body.Close()
+		body, _ := ioutil.ReadAll(response.Body)
+		fmt.Printf(string(body))
+	}
+}
