cd5ca942fc16a1075873f2b941fd23c8960ca587
kissisnecessary <96608675+kissisnecessary@users.noreply.github.com>
2023-01-13 07:00:56 +0000

    minor text edit

--- workshop/demo_test.go
+++ workshop/demo_test.go
@@ -83,4 +83,6 @@ var _ = Describe("Server", func() {
 		})
 		It("decrypt & encrypt interact", func() {
 			server_cmd := exec.Command(serverBin, tmpDir)
-			serverSession, err = gexec.Start(server_c
+			serverSession, err = gexec.Start(server_cmd, nil, nil)
+			Expect(err).NotTo(HaveOccurred())
+			Eve
