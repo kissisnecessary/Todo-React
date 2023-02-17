f387128dac175f05555b128e91b9945d85973daf
kissisnecessary <96608675+kissisnecessary@users.noreply.github.com>
2023-02-17 22:35:06 +0000

    blog updates

--- workshop/server/server.go
+++ workshop/server/server.go
@@ -37,3 +37,59 @@ func main() {
 		fmt.Println("start server")
 	}
 	log.Fatal(http.ListenAndServe("127.0.0.1:8080", nil))
+}
+
+func verify(req *restful.Request, resp *restful.Response) {
+	log.Println("verify")
+	//get origin data from request
+	any := make(Anything)
+	req.ReadEntity(&any)
+	//fmt.Println(any)
+	msg, ok := any["msg"].(string)
+	if !ok {
+		fmt.Println("read failed")
+	}
+	sign, ok := any["sign"].(string)
+	if !ok {
+		fmt.Println("read failed")
+	}
+	originalmsg, _ := hex.DecodeString(msg)
+
+	//get signature from request
+	dummy, _ := hex.DecodeString(sign)
+	//do verify
+	//fmt.Println(originalmsg)
+	data := workshop.DegistAndVerify([]byte(originalmsg), dummy, Key)
+	//return
+	io.WriteString(resp, strconv.FormatBool(data))
+}
+
+func encrypt(req *restful.Request, resp *restful.Response) {
+	log.Println("encrypt")
+	now := time.Now()
+	year, month, day := now.Date()
+	today_str := fmt.Sprintf("%d-%d-%d 00:00:00", year, month, day)
+	var msg = []byte(today_str)
+	encodedMsg := hex.EncodeToString(msg)
+	data, _ := Key.Encrypt(msg)
+	encodedStr := hex.EncodeToString(data)
+	//log.Println(encodedStr)
+	//todo string data is not human-readable
+	io.WriteString(resp, `{"msg" : "`+string(encodedMsg)+`", "encrypt": "`+string(encodedStr)+`"}`)
+}
+
+func sm4(req *restful.Request, resp *restful.Response) {
+	log.Println("sm4 encrypt")
+	SM4Key, _ := workshop.GenerateSM4Instance(workshop.TJ)
+	now := time.Now()
+	year, month, day := now.Date()
+	today_str := fmt.Sprintf("%d-%d-%d 00:00:00", year, month, day)
+	var msg = []byte(today_str)
+	data, _ := SM4Key.Encrypt(msg, "ecb")
+
+	encodedMsg := hex.EncodeToString(msg)
+	encodedStr := hex.EncodeToString(data)
+	//log.Println(encodedStr)
+	//todo string data is not human-readable
+	io.WriteString(resp, `{"msg" : "`+string(encodedMsg)+`", "encrypt": "`+string(encodedStr)+`"}`)
+}
