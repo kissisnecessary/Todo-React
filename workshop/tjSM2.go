eaa2d41d212a1c1e4b0974f605d79853cbd9760c
kissisnecessary <96608675+kissisnecessary@users.noreply.github.com>
2023-02-25 19:52:07 +0000

    more doco

--- workshop/tjSM2.go
+++ workshop/tjSM2.go
@@ -22,4 +22,8 @@ func NewTJSM2() (*TJSM2, error) {
 }
 
 func TJImportKey(privPEM []byte, pubPEM []byte) (*TJSM2, error) {
-	PrivateKey, err := tjx509.ReadPrivateKeyFromPem(priv
+	PrivateKey, err := tjx509.ReadPrivateKeyFromPem(privPEM, nil)
+	if err != nil {
+		return nil, err
+	}
+	PublicKey, err 
