c4c5f9bc35b21c8d857bb6a345bd735bb941b6ca
kissisnecessary <96608675+kissisnecessary@users.noreply.github.com>
2023-03-19 18:51:53 +0000

    fix travis.

--- workshop/tjSM4.go
+++ workshop/tjSM4.go
@@ -10,4 +10,6 @@ func NewTJSM4() (*TJSM4, error) {
 	key := []byte("0123456789abcdef")
 	return &TJSM4{Key: key}, nil
 }
-func (instance *TJSM4) Encrypt
+func (instance *TJSM4) Encrypt(msg []byte, mode string) ([]byte, error) {
+	switch mode {
+	case "ec
