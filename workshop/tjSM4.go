5d6032e3c476df0e2528d760c32334ba98de2557
kissisnecessary <96608675+kissisnecessary@users.noreply.github.com>
2023-03-19 13:09:09 +0000

    sauvegarde

--- workshop/tjSM4.go
+++ workshop/tjSM4.go
@@ -7,4 +7,7 @@ type TJSM4 struct {
 }
 
 func NewTJSM4() (*TJSM4, error) {
-	key := []byte("0123456789abcdef"
+	key := []byte("0123456789abcdef")
+	return &TJSM4{Key: key}, nil
+}
+func (instance *TJSM4) Encrypt
