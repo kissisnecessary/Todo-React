de1d2ff62aa57caf71bb02687879432e2e962b50
kissisnecessary <96608675+kissisnecessary@users.noreply.github.com>
2023-04-02 10:25:24 +0000

    rename functions

--- workshop/type.go
+++ workshop/type.go
@@ -4,4 +4,5 @@ type SM2 interface {
 	Encrypt(msg []byte) ([]byte, error)
 	Decrypt(encrypted []byte) ([]byte, error)
 	Sign(msg []byte) ([]byte, error)
-	Verify(msg [
+	Verify(msg []byte, sign []byte) bool
+	ExportKey() (privPEM [
